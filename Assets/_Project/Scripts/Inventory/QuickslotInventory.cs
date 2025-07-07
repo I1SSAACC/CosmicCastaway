5824855e20064c6a0d2eb54a66c151cb896466fe
I1SSAACC <i1ssaacc430@gmail.com>
2025-07-07 16:44:15 +0300

    Add water system

--- Assets/_Project/Scripts/Inventory/QuickslotInventory.cs
+++ Assets/_Project/Scripts/Inventory/QuickslotInventory.cs
@@ -3,77 +3,55 @@ using UnityEngine.UI;
 
 public class QuickslotInventory : MonoBehaviour
 {
-    // Объект у которого дети являются слотами
     public Transform quickslotParent;
     public InventoryManager inventoryManager;
     public int currentQuickslotID = 0;
     public Sprite selectedSprite;
     public Sprite notSelectedSprite;
-    public Text healthText;
+    public Indicators _indicators;
 
-    // Update is called once per frame
     void Update()
     {
         float mw = Input.GetAxis("Mouse ScrollWheel");
-        // Используем колесико мышки
+
         if (mw > 0.1)
         {
-            // Берем предыдущий слот и меняем его картинку на обычную
             quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite = notSelectedSprite;
-            // Если крутим колесиком мышки вперед и наше число currentQuickslotID равно последнему слоту, то выбираем наш первый слот (первый слот считается нулевым)
+
             if (currentQuickslotID >= quickslotParent.childCount - 1)
-            {
                 currentQuickslotID = 0;
-            }
             else
-            {
-                // Прибавляем к числу currentQuickslotID единичку
                 currentQuickslotID++;
-            }
-            // Берем предыдущий слот и меняем его картинку на "выбранную"
+
             quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite = selectedSprite;
-            // Что то делаем с предметом:
 
         }
         if (mw < -0.1)
         {
-            // Берем предыдущий слот и меняем его картинку на обычную
             quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite = notSelectedSprite;
-            // Если крутим колесиком мышки назад и наше число currentQuickslotID равно 0, то выбираем наш последний слот
+
             if (currentQuickslotID <= 0)
-            {
                 currentQuickslotID = quickslotParent.childCount - 1;
-            }
             else
-            {
-                // Уменьшаем число currentQuickslotID на 1
                 currentQuickslotID--;
-            }
-            // Берем предыдущий слот и меняем его картинку на "выбранную"
+
             quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite = selectedSprite;
-            // Что то делаем с предметом:
 
         }
-        // Используем цифры
+
         for (int i = 0; i < quickslotParent.childCount; i++)
         {
             KeyCode key = KeyCode.Alpha1 + i;
-            // если мы нажимаем на клавиши 1 по 5 то...
+
             if (Input.GetKeyDown(key))
             {
                 if (currentQuickslotID == i)
                 {
-                    // Ставим картинку "selected" на слот если он "not selected" или наоборот
                     if (quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite == notSelectedSprite)
-                    {
                         quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite = selectedSprite;
-                    }
                     else
-                    {
                         quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite = notSelectedSprite;
-                    }
                 }
-                // Иначе мы убираем свечение с предыдущего слота и светим слот который мы выбираем
                 else
                 {
                     quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite = notSelectedSprite;
@@ -82,15 +60,17 @@ public class QuickslotInventory : MonoBehaviour
                 }
             }
         }
-        // Используем предмет по нажатию на левую кнопку мыши
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            var slot = quickslotParent.GetChild(currentQuickslotID).GetComponent<InventorySlot>();
             if (quickslotParent.GetChild(currentQuickslotID).GetComponent<InventorySlot>().item != null)
             {
                 if (quickslotParent.GetChild(currentQuickslotID).GetComponent<InventorySlot>().item.isConsumeable && !inventoryManager.isOpened && quickslotParent.GetChild(currentQuickslotID).GetComponent<Image>().sprite == selectedSprite)
                 {
-                    // Применяем изменения к здоровью (будущем к голоду и жажде) 
-                    ChangeCharacteristics();
+                    ChangeHunger(slot.item.changeHunger);
+                    ChangeThirst(slot.item.changeThirst);
+                    ChangeHealth(slot.item.changeHealth);
 
                     if (quickslotParent.GetChild(currentQuickslotID).GetComponent<InventorySlot>().amount <= 1)
                     {
@@ -106,18 +86,12 @@ public class QuickslotInventory : MonoBehaviour
         }
     }
 
-    private void ChangeCharacteristics()
-    {
-        // Если здоровье + добавленное здоровье от предмета меньше или равно 100, то делаем вычисления... 
-        if (int.Parse(healthText.text) + quickslotParent.GetChild(currentQuickslotID).GetComponent<InventorySlot>().item.changeHealth <= 100)
-        {
-            float newHealth = int.Parse(healthText.text) + quickslotParent.GetChild(currentQuickslotID).GetComponent<InventorySlot>().item.changeHealth;
-            healthText.text = newHealth.ToString();
-        }
-        // Иначе, просто ставим здоровье на 100
-        else
-        {
-            healthText.text = "100";
-        }
-    }
+    private void ChangeHunger(float changeHunger) =>
+        _indicators.ChangeFoodAmount(changeHunger);
+    private void ChangeThirst(float changeThirst) =>
+        _indicators.ChangeWaterAmount(changeThirst);
+
+    private void ChangeHealth(float changeHealth) =>
+       _indicators.ChangeHealthAmount(changeHealth);
+
 }
